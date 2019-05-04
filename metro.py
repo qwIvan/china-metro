1247019ae94b29b4f7880f631e7ebd8edfa25cd9
Ivan <qwIvan@users.noreply.github.com>
2019-05-04 15:41:25 +0800

    update

--- metro.py
+++ metro.py
@@ -5,7 +5,17 @@ from bs4 import BeautifulSoup
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
 
-gotSid = set()
+got_sid = set()
+lines = {}
+
+
+def output_lines():
+    with open('lines.csv', 'a+') as f:
+        print('城市行政区划代码', '城市名', '线路id', '线路名', sep=',', file=f)
+        for line_id, line in lines.items():
+            print(line['cityName'], line['lineName'])
+            print(line['cityCode'], line['cityName'],
+                  line_id, line['lineName'], sep=',', file=f)
 
 
 def get_message(ID, cityname, name):
@@ -19,26 +29,35 @@ def get_message(ID, cityname, name):
     result = json.loads(html)
     #import IPython
     # IPython.embed()
-    with open('metro.csv', 'a+') as f:
+    with open('stations.csv', 'a+') as f:
         for i in result['l']:
             for j in i['st']:
                 line_name = i['ln']
                 # 判断是否含有地铁分线
                 if i['la']:
                     line_name += i['la']
-                station = {
-                    'cityCode': ID,
-                    'cityName': name,
-                    'lineId': i['ls'],
+
+                city_code = ID
+                city_name = name
+                line_id = i['ls']
+                lines[line_id] = {
+                    'lineId': line_id,
                     'lineName': line_name,
+                    'cityCode': city_code,
+                    'cityName': city_name,
+                }
+
+                station = {
+                    'cityCode': city_code,
+                    'cityName': city_name,
                     'stationId': j['sid'],
                     'stationName': j['n'],
                     'stationPosition': j['sl'],
                     'stationLines': j['r'],
                 }
-                if station['stationId'] in gotSid:
+                if station['stationId'] in got_sid:
                     continue
-                gotSid.add(station['stationId'])
+                got_sid.add(station['stationId'])
                 print(*station.values())
                 print(*station.values(), file=f, sep=',')
 
@@ -58,8 +77,8 @@ def get_city():
     res1 = soup.find_all(class_="city-list fl")[0]
     res2 = soup.find_all(class_="more-city-list")[0]
 
-    with open('metro.csv', 'a+') as f:
-        print('城市行政区划代码', '城市名', '线路id', '线路名', '地铁站名', '经度', '纬度', '所属线路',
+    with open('stations.csv', 'a+') as f:
+        print('城市行政区划代码', '城市名', '地铁站ID', '地铁站名', '经度', '纬度', '所属线路',
               sep=',', file=f)
 
     for i in res1.find_all('a'):
@@ -82,3 +101,4 @@ def get_city():
 
 if __name__ == '__main__':
     get_city()
+    output_lines()
