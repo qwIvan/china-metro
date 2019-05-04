c634937b7a982ab5bd4a8d44962818f8f6167dee
Ivan <qwIvan@users.noreply.github.com>
2019-05-04 11:16:01 +0800

    update

--- metro.py
+++ metro.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
 
+gotSid = set()
+
 
 def get_message(ID, cityname, name):
     """
@@ -15,11 +17,9 @@ def get_message(ID, cityname, name):
     response = requests.get(url=url, headers=headers)
     html = response.text
     result = json.loads(html)
-    # import IPython
+    #import IPython
     # IPython.embed()
     with open('metro.csv', 'a+') as f:
-        print('城市行政区划代码', '城市名', '线路id', '线路名', '地铁站名', '经度', '纬度', '所属线路',
-              sep=',', file=f)
         for i in result['l']:
             for j in i['st']:
                 line_name = i['ln']
@@ -31,10 +31,14 @@ def get_message(ID, cityname, name):
                     'cityName': name,
                     'lineId': i['ls'],
                     'lineName': line_name,
+                    'stationId': j['sid'],
                     'stationName': j['n'],
                     'stationPosition': j['sl'],
                     'stationLines': j['r'],
                 }
+                if station['stationId'] in gotSid:
+                    continue
+                gotSid.add(station['stationId'])
                 print(*station.values())
                 print(*station.values(), file=f, sep=',')
 
@@ -53,6 +57,11 @@ def get_city():
     # 城市列表
     res1 = soup.find_all(class_="city-list fl")[0]
     res2 = soup.find_all(class_="more-city-list")[0]
+
+    with open('metro.csv', 'a+') as f:
+        print('城市行政区划代码', '城市名', '线路id', '线路名', '地铁站名', '经度', '纬度', '所属线路',
+              sep=',', file=f)
+
     for i in res1.find_all('a'):
         # 城市ID值
         ID = i['id']
